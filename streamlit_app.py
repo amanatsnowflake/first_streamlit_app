126cee1167ba0efb305031ed1bc52345908acb56
amanatsnowflake <150109477+amanatsnowflake@users.noreply.github.com>
2023-11-06 22:55:55 -0700

    Update streamlit_app.py
    added icons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale,spinach & Rocket smoothie')
-streamlit.text('Hard boiled free-range egg')
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+streamlit.text('🥗 Kale,spinach & Rocket smoothie')
+streamlit.text('🐔 Hard boiled free-range egg')
+streamlit.text('🥑🍞 Avocado toast')
+   
