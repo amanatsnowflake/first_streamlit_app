4633a501d2428a867ba3667b687e41b7d8748499
amanatsnowflake <150109477+amanatsnowflake@users.noreply.github.com>
2023-11-06 22:57:40 -0700

    Update streamlit_app.py
    added streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
+streamlit.title('My Mom's New Healthy diner')
 streamlit.header('Breakfast menu')
 streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
 streamlit.text('🥗 Kale,spinach & Rocket smoothie')
 streamlit.text('🐔 Hard boiled free-range egg')
 streamlit.text('🥑🍞 Avocado toast')
-   
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
