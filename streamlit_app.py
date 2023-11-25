c6c962053d5fab502c09e48658de27a0f5fb3cf0
amanatsnowflake <150109477+amanatsnowflake@users.noreply.github.com>
2023-11-25 00:24:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import snowflake.connector
 streamlit.title('My Mom&apos;s New Healthy diner')
 streamlit.header('Breakfast menu')
 streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
