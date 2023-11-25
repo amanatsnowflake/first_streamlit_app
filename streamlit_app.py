6b7d637cca406200d82c30cbe31d8af5e69f7666
amanatsnowflake <150109477+amanatsnowflake@users.noreply.github.com>
2023-11-25 01:54:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,7 +58,7 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
 
 # don't run anything pst here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
 ##my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
